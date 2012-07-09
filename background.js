48ac42279cb25f5aec6534020f80117911ee1e6b
Tomasz Jackowiak <tomasz.jackowiak@gmail.com>
2012-07-09 03:33:08 +0200

    Code formatting

--- background.js
+++ background.js
@@ -3,65 +3,65 @@ var badgeColors = {2: "#A8A8A8", 7: "#007700", 15: "#dd7700", 25: "#dd0000"}
 var streams = []
 
 function setBadge(maxViewersCount) {
-	maxCount = maxViewersCount / 1000;
-	maxCount = Math.round(maxCount);
-	color = false;
+    maxCount = maxViewersCount / 1000;
+    maxCount = Math.round(maxCount);
+    color = false;
 
-	for (badge in badgeColors) {
-		if (maxCount >= badge) {
-			color = badgeColors[badge]
-		}
-	}
+    for (badge in badgeColors) {
+        if (maxCount >= badge) {
+            color = badgeColors[badge]
+        }
+    }
 
-	if (color) {
-		chrome.browserAction.setBadgeText({text: maxCount.toString() + 'k'});	
-		chrome.browserAction.setBadgeBackgroundColor({color: color});
-	}
-	else {
-		chrome.browserAction.setBadgeText({text: ''});
-	}
+    if (color) {
+        chrome.browserAction.setBadgeText({text: maxCount.toString() + 'k'});   
+        chrome.browserAction.setBadgeBackgroundColor({color: color});
+    }
+    else {
+        chrome.browserAction.setBadgeText({text: ''});
+    }
 }
 
 function prepareData(data) {
-	returnData = []
-	data.children().each(function(index) {
-		viewers = $(this).find('div>p.channelname span').text()
-		channel = $(this).find('div>p.channelname a').text()
-		img = $(this).find('a.thumb img')
-		img = img.attr('src1') ? img.attr('src1') : img.attr('src')
-		returnData.push(
-		{
-			channel: channel,
-			viewers: viewers.replace(',', ' '),
-			count: parseInt(viewers.replace(',', '')),
-			img: img,
-			title: $(this).find('div>p.title a').text(),
-			url: 'http://twitch.tv/' + channel.toLowerCase()
-		})
-	})
+    returnData = []
+    data.children().each(function(index) {
+        viewers = $(this).find('div>p.channelname span').text()
+        channel = $(this).find('div>p.channelname a').text()
+        img = $(this).find('a.thumb img')
+        img = img.attr('src1') ? img.attr('src1') : img.attr('src')
+        returnData.push(
+        {
+            channel: channel,
+            viewers: viewers.replace(',', ' '),
+            count: parseInt(viewers.replace(',', '')),
+            img: img,
+            title: $(this).find('div>p.title a').text(),
+            url: 'http://twitch.tv/' + channel.toLowerCase()
+        })
+    })
 
-	return returnData;
+    return returnData;
 }
 
 function getStreams() {
-	$('<div/>').load(twitchTvPage + " div.video",
-		function(data) {
-			streams = prepareData($(this));
-			setBadge(streams[0].count);
-		}
-	);
+    $('<div/>').load(twitchTvPage + " div.video",
+        function(data) {
+            streams = prepareData($(this));
+            setBadge(streams[0].count);
+        }
+    );
 }
 
 chrome.extension.onMessage.addListener(function(request, sender, sendResponse) {
-	if (request.getStreams) {
-		console.log("got request: getStreams");
-		sendResponse(streams.slice(0, parseInt(request.getStreams)));
-	}
+    if (request.getStreams) {
+        console.log("got request: getStreams");
+        sendResponse(streams.slice(0, parseInt(request.getStreams)));
+    }
 
 });
 
 
 $(document).ready( function() {
-	getStreams();
-	setInterval(getStreams, 60000);
+    getStreams();
+    setInterval(getStreams, 60000);
 });
--- popup.js
+++ popup.js
@@ -1,14 +1,14 @@
-chrome.extension.sendMessage({getStreams: 10}, function(response){
-  $.each(response, function(k, stream) {
-    var row = $('<div class="main"/>');
-    $('<img/>').attr({'src': stream.img}).appendTo(row);
-    $('<div/>')
-      .append($('<div/>').text(stream.viewers + ' viewers'))
-      .append($('<div/>').text(stream.channel))
-      .appendTo(row);
-    $(row)
-      .click(function() { chrome.tabs.create({url: stream.url}); })
-      .appendTo('#streamList');
-    $('<div/>').css('clear', 'both').appendTo('#streamList');
-  });
+chrome.extension.sendMessage({getStreams: 10}, function(response) {
+    $.each(response, function(k, stream) {
+        var row = $('<div class="main"/>');
+        $('<img/>').attr({'src': stream.img}).appendTo(row);
+        $('<div/>')
+            .append($('<div/>').text(stream.viewers + ' viewers'))
+            .append($('<div/>').text(stream.channel))
+            .appendTo(row);
+        $(row)
+            .click(function() { chrome.tabs.create({url: stream.url}); })
+            .appendTo('#streamList');
+        $('<div/>').css('clear', 'both').appendTo('#streamList');
+    });
 });
